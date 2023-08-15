4d67ae9783343daae007395e57e899c12b9113e8
Masanori Kusunoki <masanork@gmail.com>
2023-08-16 08:39:07 +0900

    フォントのフォールバック機構、エラー出力を追加

--- src/GlyphPicker.py
+++ src/GlyphPicker.py
@@ -10,14 +10,39 @@ with open("font.toml", "r") as file:
     DEFAULT_FONT = STYLES.get("default")
 
 def subset_font(text, style="default", output_file=None):
-    # スタイルに応じたフォントを選択
-    font_name = STYLES.get(style)
-    if not font_name:
+    # Get the list of fonts for the given style, including fallbacks
+    font_list = STYLES.get(style)
+    if not font_list:
         raise ValueError(f"Unknown font style: {style}")
-    font_path = FONT_PATH + font_name
-    
-    font = TTFont(font_path)
-    
+
+    # Load the main font (first font in the list)
+    main_font_path = FONT_PATH + font_list[0]
+    font = TTFont(main_font_path)
+
+    # Check for missing glyphs in the main font and try to fetch them from fallback fonts
+    # Exclude control characters (e.g., 0xa) from missing glyphs
+    missing_glyphs = [char for char in text if ord(char) not in font.getBestCmap() and not (0 <= ord(char) <= 0x1f)]
+
+    for fallback_font_name in font_list[1:]:
+        if not missing_glyphs:
+            break
+
+        # Load the fallback font
+        fallback_font_path = FONT_PATH + fallback_font_name
+        fallback_font = TTFont(fallback_font_path)
+
+        # Try to fetch missing glyphs from the fallback font
+        for char in missing_glyphs[:]:
+            if ord(char) in fallback_font.getBestCmap():
+                font['glyf'][char] = fallback_font['glyf'][char]
+                font['cmap'].tables[0].cmap[ord(char)] = char
+                missing_glyphs.remove(char)
+
+    # If there are still missing glyphs after checking all fallbacks, raise an error
+    if missing_glyphs:
+        missing_codepoints = [hex(ord(char)) for char in missing_glyphs]
+        print(f"Glyphs not found for codepoints: {', '.join(missing_codepoints)}")
+
     options = Options()
     options.flavor = "woff2"
     options.desubroutinize = True
